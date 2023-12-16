28bea1082b289c9f49ff381c3abd272383e1eedc
REDD <ar1k88gamingllc@gmail.com>
2023-12-16 16:59:39 -0600

    Update helloworld.ps1

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,6 +1,6 @@
 Write-Host "Hello World!"
 $test = "This is just a demo script.`nTesting Escape Characters."
-function blahblah {
+function demo-function {
   Write-Host "$test"
 }
-blahblah
+demo-function
